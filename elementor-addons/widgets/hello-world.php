f2d1aeff002495fc693db7462da64a89a78bfab4
zahidupm <zahidhasan.limon111@gmail.com>
2023-07-08 19:51:29 +0600

    hello world addons

--- elementor-addons/widgets/hello-world.php
+++ elementor-addons/widgets/hello-world.php
@@ -28,11 +28,34 @@ class ZAHID_Elementor_Addon_Hello_World extends \Elementor\Widget_Base
         return ['hello', 'world'];
     }
 
+    protected function _register_controls()
+    {
+        $this->start_controls_section(
+            'Content',
+            [
+                'label' => esc_html__('Content', 'elementor-addon'),
+                'tab' => \Elementor\Controls_Manager::TAB_CONTENT,
+            ]
+        );
+
+        $this->add_control(
+            'title',
+            [
+                'label' => esc_html__('Title', 'elementor-addon'),
+                'type' => \Elementor\Controls_Manager::TEXT,
+                'default' => esc_html__('Hello world', 'elementor-addon'),
+            ]
+        );
+
+        $this->end_controls_section();
+    }
+
     protected function render()
     {
+        $settings = $this->get_settings_for_display();
         ?>
 
-		<p> Hello World </p>
+		<p> <?php echo $settings['title']; ?> </p>
 
 		<?php
 }
