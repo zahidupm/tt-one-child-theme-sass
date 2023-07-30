8ce47dbf19d9ab21ed84329c12298955c894ea95
zahidupm <zahidhasan.limon111@gmail.com>
2023-07-30 12:17:47 +0600

    add addons

--- elementor-addons/addons.php
+++ elementor-addons/addons.php
@@ -3,9 +3,10 @@
 function zahid_tt_one_child_widgets($widgets_manager)
 {
 
-    require_once __DIR__ . '/widgets/hello-world.php';
+    require_once __DIR__ . '/widgets.php';
 
-    $widgets_manager->register(new \ZAHID_Elementor_Addon_Hello_World());
+    $widgets_manager->register(new \ZAHID_Team_Members());
+    $widgets_manager->register(new \Hover_Team_Members());
 
 }
 add_action('elementor/widgets/register', 'zahid_tt_one_child_widgets');
